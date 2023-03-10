fbdfa29a3bab5c69b7aef7376ac1bc3eb95d3e4f
Lucas Costa <lucas.alves.costa96@gmail.com>
2023-03-09 21:17:28 -0300

    Add env variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_ETHERSCAN_API_KEY: process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY
+  }
 }
 
 module.exports = nextConfig
