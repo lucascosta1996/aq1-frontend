b3620d2596a5714297d2a496785a29b8c19f9792
Lucas Costa <lucas.alves.costa96@gmail.com>
2023-03-10 17:18:50 -0300

    Delete whitepaper page

--- src/pages/whitepaper.js
+++
@@ -1,9 +0,0 @@
-import whitepaper from '@/assets/Aliquo-Whitepaper.pdf';
-
-export default function Whitepaper() {
-    return (
-        <>
-            <embed src={whitepaper} type="application/pdf" width="100%" height="600px" />
-        </>
-    )
-}
