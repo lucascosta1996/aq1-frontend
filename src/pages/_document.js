b54e175c95b8d8d807f1436a422432df46f3828b
Lucas Costa <lcosta@extensiv.com>
2023-07-23 21:57:17 -0300

    Add metadata to document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,27 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <title>Aliquo</title>
+          <meta name="google" content="notranslate" />
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="https://aliquo.xyz/" />
+          <meta name="twitter:title" content="Aliquo" />
+          <meta name="twitter:description" content="A decentralized reserve asset protocol, built on Ethereum." />
+          <meta name="twitter:image" content="https://files.gitbook.com/v0/b/gitbook-x-prod.appspot.com/o/spaces%2F0KTkDWgsWbKjzAKNB4OG%2Fuploads%2FlETM2l0cC8DfOyXfDiIR%2Fimage.png?alt=media&token=6beafb61-0c31-45b7-b1a0-6db24873d0dd" />
+          
+          <meta property="og:type" content="Aliquo" />
+          <meta property="og:url" content="https://aliquo.xyz/" />
+          <meta property="og:title" content="Aliquo" />
+          <meta property="og:description" content="A decentralized reserve asset protocol, built on Ethereum." />
+          <meta property="og:image" content="https://files.gitbook.com/v0/b/gitbook-x-prod.appspot.com/o/spaces%2F0KTkDWgsWbKjzAKNB4OG%2Fuploads%2FlETM2l0cC8DfOyXfDiIR%2Fimage.png?alt=media&token=6beafb61-0c31-45b7-b1a0-6db24873d0dd" />
+
+          <meta name="description" content="A decentralized reserve asset protocol, built on Ethereum." />
+          <meta name="google-site-verification" content="+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34=" />
+          <meta name="robots" content="https://files.gitbook.com/v0/b/gitbook-x-prod.appspot.com/o/spaces%2F0KTkDWgsWbKjzAKNB4OG%2Fuploads%2FlETM2l0cC8DfOyXfDiIR%2Fimage.png?alt=media&token=6beafb61-0c31-45b7-b1a0-6db24873d0dd" />
+      </Head>
       <body>
         <Main />
         <NextScript />
